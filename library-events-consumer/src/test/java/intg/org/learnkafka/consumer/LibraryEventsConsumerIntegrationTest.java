4274dc1fe03fc6d8ad4f19c495405ffba101146c
sandeep-simplify <sandeep@simplifymoney.in>
2024-04-21 15:50:42 +0530

    Added Default Error Handling

--- library-events-consumer/src/test/java/intg/org/learnkafka/consumer/LibraryEventsConsumerIntegrationTest.java
+++ library-events-consumer/src/test/java/intg/org/learnkafka/consumer/LibraryEventsConsumerIntegrationTest.java
@@ -140,8 +140,8 @@ public class LibraryEventsConsumerIntegrationTest {
         latch.await(5, TimeUnit.SECONDS);
 
         // then
-        verify(libraryEventsConsumerSpy, times(10)).onMessage(isA(ConsumerRecord.class));
-        verify(libraryEventServiceSpy, times(10)).processLibraryEvent(isA(ConsumerRecord.class));
+        verify(libraryEventsConsumerSpy, times(3)).onMessage(isA(ConsumerRecord.class));
+        verify(libraryEventServiceSpy, times(3)).processLibraryEvent(isA(ConsumerRecord.class));
 
     }
 }
